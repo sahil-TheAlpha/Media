44f6915bcbdd246b19104d98116e317aeb13d2a1
Sahil Sachdeva <ssachdeva678@gmail.com>
2019-02-22 23:23:54 +0530

    CTA Position Update

--- JS/index.js
+++ JS/index.js
@@ -5,15 +5,6 @@ function closeSideMenu(){
 	document.getElementById('sideMenu').style.width = '0%';
 }
 
-// Opera 8.0+
-var opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
-
-// Firefox 1.0+
-var firefox = navigator.userAgent.indexOf('Firefox') > -1;
-
-// Chrome 1 - 71
-var chrome = navigator.userAgent.indexOf('Chrome') > -1;
-
 //Modal
 var modal = document.getElementById('myModal');
 
@@ -21,16 +12,53 @@ var modal = document.getElementById('myModal');
 var modalDialog = document.getElementsByClassName('modal-dialog');
 
 //CTA Button 
-var button = document.getElementById('logo');
+var button = document.getElementById('btnCTA');
 
+var ba =["Chrome","Safari","Firefox","Opera","MSIE","Trident"];
+
+var browser;
+
+var ua = navigator.userAgent;
+
+var edge = /Edge/.test(navigator.userAgent);
+
+for(var i = 0; i < ba.length; i++){
+	if(ua.indexOf(ba[i]) > -1){
+    	browser = ba[i];
+        break;
+    }
+}
+
+//Button Event Listener
 button.addEventListener('click', function(){
-	if(chrome){
-		modal.classList.add('modal-chrome');
+	if(/Edge/.test(navigator.userAgent)){
+	    // alert("Hi MS User");
+	    modal.style.position = "absolute";
+	    modal.style.right = "0%";
+	    modal.style.top = "25%";
+	    modal.style.width = "30%";
 	}
-	else if(opera){
-		modal.classList.add('modal-opera');
+	else if(browser == "Chrome"){
+		// alert("Hi Chrome User");
+		modal.style.position = "absolute";
+	    modal.style.right = "0%";
+	    modal.style.top = "0%";
+	    modal.style.width = "30%";
 	}
-	else if(firefox){
-		modal.classList.add('modal-firefox');
+	else if(browser == "Firefox"){
+		// alert("Hi Firefox User")
+		modal.style.position = "absolute";
+	    modal.style.left = "0%";
+	    modal.style.top = "0%";
+	    modal.style.width = "30%";	
 	}
+	else if(browser == "Safari"){
+		modal.style.position = "absolute";
+	    modal.style.left = "0%";
+	    modal.style.top = "25%";
+	    modal.style.width = "30%";	
+	}
+	else if(browser == "Opera"){
+		alert("Hi Opera User");
+	}	
 });
